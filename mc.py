b832436035d81e11c29f7aacd66ae3d2633ab4b4
Seb <32842507+Sebiann@users.noreply.github.com>
2018-07-26 16:03:31 +0200

    Add files via upload

---
+++ mc.py
@@ -0,0 +1,15 @@
+import subprocess
+import discord
+from discord.ext import commands
+
+class Mc:
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def start(self)
+        subprocess.call("start.sh", shell=True)
+        await self.client.say('Server Started')
+
+def setup(client):
+    client.add_cog(Mc(client))
