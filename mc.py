490b0bfa3b1a1ea846d6c14eccf0a93939c47336
Seb <32842507+Sebiann@users.noreply.github.com>
2018-07-28 19:19:27 +0200

    Hope it works now

--- mc.py
+++ mc.py
@@ -9,13 +9,14 @@ class Mc:
     def __init__(self, client):
         self.client = client
 
-    @self.client.event
-    async def on_message(message):
-        if message.content.startswith('zstart') and any(role.name == name for role in message.author.roles):
+    @commands.command()
+    async def start(self, ctx):
+        author = ctx.message.author
+        if (any(role.name == 'name' for role in author.roles)):
             subprocess.call("start.sh", shell=True)
             await self.client.say('Server started')
         else:
-            await self.client.say('Sorry No permissions')
+            await self.client.say('Sorry no permissions')
 
 def setup(client):
     client.add_cog(Mc(client))
