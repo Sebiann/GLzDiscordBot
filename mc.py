5488a3dac01174271719594dc65bbfb8f73b759e
Seb <32842507+Sebiann@users.noreply.github.com>
2018-07-28 08:25:03 +0200

    Changed some stuff, hoping it will work

--- mc.py
+++ mc.py
@@ -2,23 +2,18 @@ import subprocess
 import discord
 from discord.ext import commands
 
-owner = '187239212544688128' #Use: if message.author.id == owner:
+owner = '187239212544688128'
 atlantic = '465504814155956225'
 
 class Mc:
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def start(self):
-        subprocess.call("start.sh", shell=True)
-        await self.client.say('Server Started')
-
-    @commands.command()
-    async def stop(self):
-        if message.author.id == owner:
-            subprocess.call("stop.sh", shell=True)
-            await self.client.say('Server Stoped')
+    @commands.event
+    async def on_message(message):
+        if message.content.startswith('zstart') and any(role.name == name for role in message.author.roles):
+            subprocess.call("start.sh", shell=True)
+            await self.client.say('Server started')
         else:
             await self.client.say('Sorry No permissions')
 def setup(client):
