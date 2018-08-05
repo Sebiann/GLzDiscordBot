3eb3e0676f8b458d3c283eb807f95ca0a27886d2
Seb <32842507+Sebiann@users.noreply.github.com>
2018-08-05 21:49:36 +0200

    Changed some permission things

--- mc.py
+++ mc.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 owner = '187239212544688128'
 hope = '465504814155956225'
+alex = '453613035387486232'
 
 class Mc:
     def __init__(self, client):
@@ -12,11 +13,20 @@ class Mc:
     @commands.command(pass_context=True)
     async def start(self, ctx):
         author = ctx.message.author
-        if (any(role.id == hope for role in author.roles)):
+        if author.id == alex:
             subprocess.call("start.sh", shell=True)
             await self.client.say('Server started')
         else:
             await self.client.say('Sorry no permissions')
+
+#    @commands.command(pass_context=True)
+#    async def start(self, ctx):
+#        author = ctx.message.author
+#        if (any(role.id == hope for role in author.roles)):
+#            subprocess.call("start.sh", shell=True)
+#            await self.client.say('Server started')
+#        else:
+#            await self.client.say('Sorry no permissions')
 
 def setup(client):
     client.add_cog(Mc(client))
