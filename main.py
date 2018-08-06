6f456d62a5c8b8ae1d2f2571781dac93b60c39a7
Seb <32842507+Sebiann@users.noreply.github.com>
2018-08-06 19:19:44 +0200

    Put in permissions in echo and ping

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import discord
 from discord.ext import commands
 
+owner = '187239212544688128'
+cmds = '424617816025464832'
+
 class Main:
     def __init__(self, client):
         self.client = client
 
     @commands.command()
-    async def ping(self):
-        await self.client.say('Pong!')
+    async def ping(self, ctx):
+        author = ctx.message.author
+        channel = ctx.message.channel
+        if channel.id == cmds or author.id == owner:
+            await self.client.say('Pong!')
+        else:
 
     @commands.command()
-    async def echo(self, *args):
-        output = ''
-        for word in args:
-            output += word
-            output += ' '
-        await self.client.say(output)
+    async def echo(self, *args, ctx):
+        author = ctx.message.author
+        channel = ctx.message.channel
+            if channel.id == cmds or author.id == owner:
+                output = ''
+                for word in args:
+                    output += word
+                    output += ' '
+                    await self.client.say(output)
+            else:
 
 def setup(client):
     client.add_cog(Main(client))
